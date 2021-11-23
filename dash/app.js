f20e3132c11905e27b54362e42a6ac157dff8ca5
Caspar Wrede <caspar.wrede@gmail.com>
2021-11-23 20:27:21 +0100

    Added new events

--- dash/app.js
+++ dash/app.js
@@ -25,8 +25,8 @@ const dinkyApp = {
                 {image: "", title: "🎄", date: "12/24/2021"},
                 {image: "", title: "🎅", date: "12/06/2021"},
                 {image: "", title: "🍰", date: "12/04/2021"},
-                {image: "", title: "🏊‍♀️", date: "11/23/2021"},
-                {image: "", title: "👯‍♀️", date: "11/30/2021"},
+                {image: "", title: "🏊‍♀️", date: "12/08/2021"},
+                {image: "", title: "👯‍♀️", date: "12/02/2021"},
             ]
         }
     },
