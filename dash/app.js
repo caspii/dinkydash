294f4eff48a07155fe659a3009332b2b4e504f09
Caspar Wrede <caspar.wrede@gmail.com>
2023-04-04 08:40:14 +0200

    Fix

--- dash/app.js
+++ dash/app.js
@@ -45,7 +45,7 @@ const dinkyApp = {
                 {image: "", title: "🐕‍", date: "04/22/2023"},
                 {image: "", title: "⛵️", date: "08/12/2023"},
                 {image: "", title: "🇪🇸", date: "7/12/2022"},
-                {image: "", title: "👩🏾‍🏫", date: "8/22/2022"},
+                {image: "", title: "🏫", date: "8/22/2022"},
 
                 // Seasons
                 {image: "", title: "🍁", date: "9/23/2022"},
